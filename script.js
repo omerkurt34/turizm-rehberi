67acc4e848d017f2769098a5e2169060e2684a55
omerkurt34 <144345460+omerkurt34@users.noreply.github.com>
2025-10-13 22:13:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const languageScreen = document.getElementById("language-screen");
+const mainScreen = document.getElementById("main-screen");
+const mainTitle = document.getElementById("main-title");
+const categoriesContainer = document.querySelector(".categories");
+
+const categories = {
+  tr: [
+    { icon: "fa-shirt", name: "Giyim Yerleri" },
+    { icon: "fa-utensils", name: "Restoranlar" },
+    { icon: "fa-mug-hot", name:
